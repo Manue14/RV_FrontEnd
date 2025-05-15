20ccad1ec40193c725dea4e25c7c7fe8536bcd60
Joel <joelfigueiridomolares@gmail.com>
2025-05-15 12:06:03 +0200

    feat:Create combox component and visualize data from api

--- src/Test.vue
+++ src/Test.vue
@@ -1,4 +1,10 @@
 <script setup>
+  import { ref, onMounted } from 'vue';
+  import Combox from "@/components/Combox.vue";
+
+  const familias = ref([]);
+  const provincias = ref([]);
+
   async function getFamilias() {
     const url = "http://127.0.0.1:5000/familias";
     try {
@@ -7,16 +13,39 @@
         throw new Error(`Response status: ${response.status}`)
       }
       const json = await response.json();
-      console.log("Hola");
-      console.log(json);
+      familias.value = json;
     } catch (error) {
       console.error(error.message);
     }
   }
-  getFamilias();
+
+  async function getProvincias() {
+    const url = "http://127.0.0.1:5000/provincias";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+      const json = await response.json();
+      provincias.value = json;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  onMounted(() => {
+    getFamilias();
+    getProvincias();
+  });
 </script>
 
 <template>
+  <div>
+    <Combox :data_list="familias"></Combox>
+  </div>
+  <div>
+    <Combox :data_list="provincias"></Combox>
+  </div>
 
 </template>
 
---
+++ src/components/Combox.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  data_list: {
+    type: JSON,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <select>
+    <option v-for="(value, key) in data_list"
+            :key="key" :value="key">
+                {{value}}</option>
+  </select>
+</template>
+<style scoped>
+
+</style>
